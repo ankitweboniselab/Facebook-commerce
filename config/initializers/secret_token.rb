79b52d2a88945087395bd16f0f794168b1366ee4
ankit <ankit@weboniselab.com>
2011-10-03 17:57:39 +0530

    ecommerce project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommerceApp::Application.config.secret_token = 'ea8b929281ef063795d60d84e1d66ffd0a49b6b10dc6f88d0c266772bf93751d5fd2fea59fb67ec2c1ae2136b30b855e7902101210a67eb7ce4a1b7a59119db4'
